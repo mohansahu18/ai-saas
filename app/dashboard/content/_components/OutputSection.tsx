6cd508afa98eb8b3563edb90865ec5fd80ef7bcf
mohan lal sahu <mohansahu18111811@gmailcom>
2024-11-01 23:25:25 +0530

    editor added

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,8 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
+import "@toast-ui/editor/dist/toastui-editor.css";
 
-const OutputSection = (props) => {
-  return <div>OutputSection</div>;
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
+const OutputSection = () => {
+  const editorRef: any = useRef();
+  return (
+    <div className="bg-white shadow-lg border rounded-lg ">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="font-medium text-lg">Your Result</h2>
+        <Button className="flex gap-2 ">
+          <Copy className="h-4 w-4" /> copy
+        </Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue="Your Result Will Appear Here"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+        height="600px"
+        onChange={() => console.log(editorRef.current.getInstance())}
+      />
+    </div>
+  );
 };
 
 OutputSection.propTypes = {};
