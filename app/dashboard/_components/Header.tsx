2c84dfb43f6da5a72f0c47ab7df1aeb4768ac533
mohan lal sahu <mohansahu18111811@gmailcom>
2024-11-03 23:11:43 +0530

    navigation added

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,20 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { Search } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className="flex justify-between items-center p-5 shadow-md border-b-2 bg-white">
-      <div className="flex gap-2 items-center p-2 border rounded-md max-w-md">
+      {/* <div className="flex gap-2 items-center p-2 border rounded-md max-w-md">
         <Search />
         <input type="text" className="outline-none" placeholder="Search...." />
-      </div>
+      </div> */}
       <hr className="my-6 border" />
       <div className="flex items-center gap-5">
-        <h2 className="bg-primary p-3 rounded-full text-xs text-white px-2 m-3">
-          {" "}
-          🔥 Join The Membership Now
-        </h2>
+        <Link href={"/dashboard/billing"}>
+          <h2 className="bg-primary p-3 rounded-full text-xs text-white px-2 m-3">
+            {" "}
+            🔥 Join The Membership Now
+          </h2>
+        </Link>
         <UserButton />
       </div>
     </div>
--- app/dashboard/_components/SearchSection.tsx
+++ app/dashboard/_components/SearchSection.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 function SearchSection({ onSearchInput }: any) {
   return (
-    <div className="flex flex-col justify-center gap-1 items-center p-10 m-2 rounded-md bg-gradient-to-tr to-[#89D1D0] via-[#8E84C8] from-[#d88095]">
+    <div className="flex flex-col justify-center gap-1 items-center p-10 m-2 rounded-md bg-gradient-to-tr from-[#704ef8] via-[#6665fb] to-[#7096f1]">
       <h2 className="text-white text-3xl font-bold">Brows All Templates</h2>
       <p className="text-white">What would you like to create today?</p>
       <div className="w-full flex items-center justify-center">
